b27b29b6d419e07f334844e90616ec644fffbbad
phanikumaralakam <154543563+phanikumaralakam@users.noreply.github.com>
2024-07-19 23:24:23 +0530

    Delete RESUME directory

--- RESUME/script.js
+++
@@ -1,8 +0,0 @@
-<p>alakamphani599@gmail.com | 8121251534 | <a href="https://www.linkedin.com/in/alakam-phani-kumar" target="_blank">LinkedIn</a></p>
-            <h2>Projects <a href="https://github.com/phanikumaralakam/alakam-phani-kumar.devtern" target="_blank">GitHub</a></h2><br>
-<li><a href="https://drive.google.com/file/d/1tAJYpbuQIsJ7abew0PR7eLVbc4wlcoB6/view" target="_blank">Machine Learning Internship - Devtern</a></li> 
-                <li><a href="https://drive.google.com/file/d/1fcDkbRG2wTxKvEKs30dnd6zSW_udMdgv/view" target="_blank">ARTIFICIAL INTELLIGENCE -  APSSDC</a></li>
-                <li><a href="https://drive.google.com/file/d/1lfYMLuzpBh7ea9suJDTxPXgu520mgq90/view">THE ULTIMATE GUIDE TO LEARN SQL FROM SCRATCH - UDEMY</a></li>
-                <li><a href="https://drive.google.com/file/d/1lfYMLuzpBh7ea9suJDTxPXgu520mgq90/view" target="_blank">PYTHON - GUVI (Google For Education Partner)</a></li>
-
-<li><a href="https://drive.google.com/file/d/1lhqn3qurPo5nd9Dn5ZlDJF-RMFKaaXGI/view" target="_blank">FOUNDATIONS ASSOCIATE - ORACLE UNIVERSITY</a></li>
